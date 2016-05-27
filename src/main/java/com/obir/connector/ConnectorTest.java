bd4d975cee6256c82558cd523c8a578987ed8bcd
Anonymous <anonymous@noemail.com>
2016-05-27 06:35:22 +0000

    More buttons and Text views for status and response

--- src/main/java/com/obir/connector/ConnectorTest.java
+++ src/main/java/com/obir/connector/ConnectorTest.java
@@ -1,98 +1,107 @@
 package com.obir.connector;
-
+ 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
-import android.os.AsyncTask;
-
+ 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.io.*;
+ 
 import javax.net.ssl.HttpsURLConnection;
-
-
-public class ConnectorTest extends Activity implements OnClickListener {
-    TextView txt;
-    Button   btn;
-
+ 
+public class ConnectorTest extends Activity implements OnClickListener
+{
+    Button btn1;
+    Button btn2;
+    TextView txt1;
+    TextView txt2;
+ 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        txt = (TextView)findViewById(R.id.txt);
-        btn = (Button)findViewById(R.id.btn);
-
-        btn.setOnClickListener(this);
-
+ 
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+        txt1 = (TextView)findViewById(R.id.txt1);
+        txt2 = (TextView)findViewById(R.id.txt2);
+ 
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+ 
     }
-
+ 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn:
+    public void onClick (View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.btn1:
                 new getData().execute("");
+                break;
+            case R.id.btn2:
+                txt2.setText("");
+                txt1.setText("Press Get Data");
                 break;
         }
     }
-  
-  private class getData extends AsyncTask<String, Void, String> {
-    
-    String request = "";
-    String inputLine;
-
+ 
+    private class getData extends AsyncTask<String, Void, String> {
+ 
+        String request = "";
+        String inputLine;
+ 
         @Override
         protected String doInBackground(String... params)
         {
-           try              
-              {             
+            try
+            {
                 String httpsURL = "https://api.forecast.io/forecast/3c70d6ecf5e1fdb93b6fbaed616daec6/49,19?units=si&exclude=minutely,hourly,alerts,flags,daily";
                 URL myurl = new URL(httpsURL);
                 HttpsURLConnection con = (HttpsURLConnection)myurl.openConnection();
                 InputStream ins = con.getInputStream();
                 InputStreamReader isr = new InputStreamReader(ins);
                 BufferedReader in = new BufferedReader(isr);
-
-             
+ 
+ 
                 while ((inputLine = in.readLine()) != null)
-                      {
-                       request = request + inputLine;
-                       }
-                in.close();              
-                } 
-          
-          catch (Exception e) 
-          {
-              Thread.interrupted();
-          }
-                      
+                {
+                    request = request + inputLine;
+                }
+                in.close();
+            }
+ 
+            catch (Exception e)
+            {
+                Thread.interrupted();
+            }
+ 
             return "Executed";
         }
-
+ 
         @Override
         protected void onPostExecute(String result) {
-          
-            txt.setText(request);
-
+ 
+            txt2.setText(request);
+            txt1.setText("Updated");
+ 
         }
-
+ 
         @Override
         protected void onPreExecute() {
-          
-          txt.setText("Updating...");
+ 
+            txt1.setText("Updating...");
         }
-
+ 
         @Override
         protected void onProgressUpdate(Void... values) {}
     }
   
 }
-
-
-
-//http://stackoverflow.com/questions/9671546/asynctask-android-example
-//http://alvinalexander.com/blog/post/java/simple-https-example
-//https://developer.android.com/reference/android/os/AsyncTask.html
